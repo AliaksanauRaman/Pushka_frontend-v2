96b9af26edee9ce4456d485fb8b9f54784ff15c6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-23 15:50:19 +0200

    Provide http client globally in the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, NgZone, ɵNoopNgZone } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const APP_CONFIG: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     // TODO: A temporary workaround, the issue:
     // https://github.com/angular/angular/issues/47538
     {
