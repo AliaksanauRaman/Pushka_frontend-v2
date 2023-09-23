ff4d56debe8f186e3c86977614b30fce5589ebfd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-23 23:03:35 +0200

    Add NgxsModule and NgxsLoggerPluginModule to APP_CONFIG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
+import { STORE } from './store';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const APP_CONFIG: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      NgxsModule.forRoot(STORE, { developmentMode: !environment.isProduction }),
+      NgxsLoggerPluginModule.forRoot({
+        collapsed: false,
+        disabled: environment.isProduction,
+      })
+    ),
+    provideRouter(routes),
+  ],
 };
