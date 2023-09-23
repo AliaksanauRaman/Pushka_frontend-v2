af3180ae4b8404a409cead28c5a6d228575fea41
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-23 21:39:08 +0200

    Remove NgZode dependency replacement

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,9 @@
-import { ApplicationConfig, NgZone, ɵNoopNgZone } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const APP_CONFIG: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    // TODO: A temporary workaround, the issue:
-    // https://github.com/angular/angular/issues/47538
-    {
-      provide: NgZone,
-      useClass: ɵNoopNgZone,
-    },
-  ],
+  providers: [provideRouter(routes), provideHttpClient()],
 };
