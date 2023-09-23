20bcf514871d7248d27ccd06b8eab88375ab492e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-23 13:06:01 +0200

    Fix the issue with disabling zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgZone, ɵNoopNgZone } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+export const APP_CONFIG: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    // TODO: A temporary workaround, the issue:
+    // https://github.com/angular/angular/issues/47538
+    {
+      provide: NgZone,
+      useClass: ɵNoopNgZone,
+    },
+  ],
 };
