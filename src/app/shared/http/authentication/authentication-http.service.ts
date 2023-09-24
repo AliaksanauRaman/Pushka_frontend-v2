402823edc0e6a9ec5e8b2ed33f6c22e54ec9bc72
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-24 20:55:25 +0200

    Create shared AuthenticationHttpService, implement its login method and create default tests

---
+++ src/app/shared/http/authentication/authentication-http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+import { BaseHttpService } from '@shared/base/base-http.service';
+import { LoginDto } from '@shared/dtos/login.dto';
+import { LoginResponseData } from '@shared/types/login-response-data';
+import { loginResponseData } from '@shared/external-types-parsers/login-response-data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationHttpService extends BaseHttpService {
+  public login(loginDto: LoginDto): Observable<LoginResponseData> {
+    return this._httpClient
+      .post<unknown>(`${this._apiUrl}/api/login`, loginDto.toMap())
+      .pipe(map((responseData) => loginResponseData.parse(responseData)));
+  }
+}
