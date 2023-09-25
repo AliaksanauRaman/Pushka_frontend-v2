0a5b7fa5a02752f5666b3860089ee01ff3e6f86b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-25 13:39:32 +0200

    Add UserState to STORE

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,4 +1,5 @@
 import { PlacesState } from './places';
 import { LocalizationsState } from './localizations';
+import { UserState } from './user';
 
-export const STORE = [PlacesState, LocalizationsState];
+export const STORE = [PlacesState, LocalizationsState, UserState];
