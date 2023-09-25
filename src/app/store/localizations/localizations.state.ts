9cea83e9c5459a8232f3a9813c6b43a0988c3440
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-25 09:00:38 +0200

    Add @Selector()-s to LocalizationsState

--- src/app/store/localizations/localizations.state.ts
+++ src/app/store/localizations/localizations.state.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Action, State, StateContext } from '@ngxs/store';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
 
 import { LocalizationsStateModel } from './model';
 import { LOCALIZATIONS_DEFAULTS } from './defaults';
 import { SelectLocalization } from './localizations.actions';
+import { Localization } from '@shared/types/localization';
 
 type StateModel = LocalizationsStateModel;
 
@@ -15,6 +16,16 @@ type StateModel = LocalizationsStateModel;
   providedIn: 'root',
 })
 export class LocalizationsState {
+  @Selector()
+  public static list(state: StateModel): ReadonlyArray<Localization> {
+    return state.list;
+  }
+
+  @Selector()
+  public static selected(state: StateModel): Localization {
+    return state.selected;
+  }
+
   @Action(SelectLocalization)
   public selectLocalization(
     context: StateContext<StateModel>,
