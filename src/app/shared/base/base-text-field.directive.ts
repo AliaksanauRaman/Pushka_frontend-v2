160666e08a0d221826e6640f0551c0a1fd340639
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-25 10:29:18 +0200

    Create shared BaseTextFieldDirective

---
+++ src/app/shared/base/base-text-field.directive.ts
@@ -0,0 +1,45 @@
+import { Directive, Input, inject, signal } from '@angular/core';
+import { z } from 'zod';
+
+import { IdDirective } from '@shared/directives/id.directive';
+import { LabelDirective } from '@shared/directives/label.directive';
+import { PlaceholderDirective } from '@shared/directives/placeholder.directive';
+
+import { BaseReactiveField } from './base-reactive-field';
+
+@Directive({
+  hostDirectives: [
+    { directive: IdDirective, inputs: ['puId'] },
+    { directive: LabelDirective, inputs: ['puLabel'] },
+    { directive: PlaceholderDirective, inputs: ['puPlaceholder'] },
+  ],
+})
+export abstract class BaseTextFieldDirective extends BaseReactiveField<string> {
+  @Input()
+  public set puMaxLength(value: number) {
+    this._maxLength.set(value);
+  }
+
+  protected readonly _idDirective = inject(IdDirective);
+  protected readonly _labelDirective = inject(LabelDirective);
+  protected readonly _placeholderDirective = inject(PlaceholderDirective);
+  protected readonly _maxLength = signal(30);
+  protected readonly _value = signal('');
+
+  public override writeValue(value: unknown): void {
+    this._value.set(z.string().parse(value));
+  }
+
+  protected handleFieldInput(value: string): void {
+    if (!this.checkIsNewValue(value)) {
+      return;
+    }
+
+    this._value.set(value);
+    this.onChange(value);
+  }
+
+  protected checkIsNewValue(value: string): boolean {
+    return this._value() !== value;
+  }
+}
