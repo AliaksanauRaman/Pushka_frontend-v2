b2f96db2099ab3222e76e15ce2c3a6ef7c30e23d
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-26 08:48:29 +0200

    Create shared BaseDropdownFieldDirective

---
+++ src/app/shared/base/base-dropdown-field.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, inject, signal } from '@angular/core';
+
+import { PlaceholderDirective } from '@shared/directives/placeholder.directive';
+
+import { BaseReactiveField } from './base-reactive-field';
+
+@Directive()
+export abstract class BaseDropdownFieldDirective<
+  T
+> extends BaseReactiveField<T> {
+  protected readonly _placeholderDirective = inject(PlaceholderDirective);
+  protected readonly _isPanelOpened = signal(false);
+
+  protected openPanel(): void {
+    this._isPanelOpened.set(true);
+  }
+
+  protected closePanel(): void {
+    this._isPanelOpened.set(false);
+  }
+
+  protected togglePanel(): void {
+    if (this._isPanelOpened()) {
+      this.closePanel();
+    } else {
+      this.openPanel();
+    }
+  }
+}
