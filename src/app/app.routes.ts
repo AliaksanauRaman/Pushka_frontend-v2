738d26ab98bbbe0e603353e0ee96c3264ad786c4
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-06 14:00:26 +0200

    Add NotFoundPageComponent to the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./core/pages/not-found-page/not-found-page.component').then(
+        (c) => c.NotFoundPageComponent
+      ),
+  },
+];
