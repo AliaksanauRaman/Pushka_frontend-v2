4656ae55bd4c687d113e837e1d279a695544a38b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 17:48:51 +0200

    Make a use of CreateDeliveryOfferFormComponent

--- src/app/deliver-parcel/pages/deliver-parcel-page/deliver-parcel-page.component.ts
+++ src/app/deliver-parcel/pages/deliver-parcel-page/deliver-parcel-page.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { CreateDeliveryOfferFormComponent } from '../../components/create-delivery-offer-form/create-delivery-offer-form.component';
+
 @Component({
   selector: 'pu-deliver-parcel-page',
   templateUrl: './deliver-parcel-page.component.html',
   styleUrls: ['./deliver-parcel-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [CreateDeliveryOfferFormComponent],
 })
 export class DeliverParcelPageComponent {}
