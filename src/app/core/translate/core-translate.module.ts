a2b2944f0708c94250011d36e1966b246dd0d29a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 15:27:45 +0200

    Enhance CoreTranslateModule, determine default langauge based on local storage and browser settings

--- src/app/core/translate/core-translate.module.ts
+++ src/app/core/translate/core-translate.module.ts
@@ -2,11 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+import { determineLanguageBasedOnBrowserLanguage } from '@shared/utils/determine-language-based-on-browser-language';
+import { determineLanguageBasedOnOneFromLocalStorage } from '@shared/utils/determine-language-based-on-one-from-local-storage';
+import { LocalStorageKey } from '@shared/enums/local-storage-key.enum';
 import { Language } from '@shared/enums/language.enum';
 
 export const CoreTranslateModule = TranslateModule.forRoot({
-  defaultLanguage: Language.BELARUSIAN,
-  useDefaultLang: true,
+  defaultLanguage:
+    determineLanguageBasedOnBrowserLanguage(window.navigator.language) ||
+    determineLanguageBasedOnOneFromLocalStorage(
+      window.localStorage.getItem(LocalStorageKey.LANGUAGE)
+    ) ||
+    Language.BELARUSIAN,
   loader: {
     provide: TranslateLoader,
     useFactory: translateLoaderFactory,
@@ -15,5 +22,5 @@ export const CoreTranslateModule = TranslateModule.forRoot({
 });
 
 function translateLoaderFactory(httpClient: HttpClient): TranslateLoader {
-  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(httpClient, '/assets/i18n/', '.json');
 }
