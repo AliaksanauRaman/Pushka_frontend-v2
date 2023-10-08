dcc14e8a1c99f8842b9b70e342d7964c4befa2a7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 14:29:05 +0200

    Make Localization comparable

--- src/app/shared/types/localization.ts
+++ src/app/shared/types/localization.ts
@@ -1,10 +1,19 @@
+import { IComparable } from '@shared/interfaces/comparable';
 import { Language } from '@shared/enums/language.enum';
 import { Locale } from '@shared/enums/locale.enum';
 
-export class Localization {
+export class Localization implements IComparable<Localization> {
   constructor(
     public readonly label: string,
     public readonly language: Language,
     public readonly locale: Locale
   ) {}
+
+  public equalsTo({ label, language, locale }: Localization): boolean {
+    return (
+      this.label === label &&
+      this.language === language &&
+      this.locale === locale
+    );
+  }
 }
