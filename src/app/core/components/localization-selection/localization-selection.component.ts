f6e977b32e8c613f85435e233afa7aa5181fbce1
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 14:26:55 +0200

    Make LocalizationSelectionComponent as a dumb component

--- src/app/core/components/localization-selection/localization-selection.component.ts
+++ src/app/core/components/localization-selection/localization-selection.component.ts
@@ -1,14 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  signal,
+} from '@angular/core';
 import { NgFor } from '@angular/common';
 import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
-import { Store } from '@ngxs/store';
 
-import { LOCALIZATIONS } from '@shared/dependencies/localizations';
-
-import {
-  SelectedLocalizationState,
-  SelectLocalization,
-} from '@store/selected-localization';
 import { Localization } from '@shared/types/localization';
 
 @Component({
@@ -20,21 +20,26 @@ import { Localization } from '@shared/types/localization';
   imports: [NgFor, CdkMenuTrigger, CdkMenu, CdkMenuItem],
 })
 export class LocalizationSelectionComponent {
-  private readonly _store = inject(Store);
-  protected readonly _localizations = inject(LOCALIZATIONS);
+  @Input({ required: true })
+  public set localizations(value: ReadonlyArray<Localization>) {
+    this._localizations.set(value);
+  }
 
-  protected readonly _selectedLocalization$ = this._store.select(
-    SelectedLocalizationState.stream
-  );
+  @Input({ required: true })
+  public set selectedLocalization(value: Localization) {
+    this._selectedLocalization.set(value);
+  }
+
+  @Output()
+  public readonly select = new EventEmitter<Localization>();
+
+  protected readonly _localizations = signal<ReadonlyArray<Localization>>([]);
+  protected readonly _selectedLocalization = signal<Localization | null>(null);
 
   protected trackByLocalizationLabel(
     _: number,
     localization: Localization
   ): string {
     return localization.label;
-  }
-
-  protected handleLocalizationSelect(localization: Localization): void {
-    this._store.dispatch(new SelectLocalization(localization));
   }
 }
