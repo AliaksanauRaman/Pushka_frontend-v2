ecd321a0d91900dba7a7f74c6432f852fc8a7895
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 11:44:23 +0200

    Create very basic routes configuration with base and wildcard routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,31 +2,49 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'send-parcel',
+    path: '',
     loadComponent: () =>
-      import(
-        './send-parcel/pages/send-parcel-page/send-parcel-page.component'
-      ).then((c) => c.SendParcelPageComponent),
-  },
-  {
-    path: 'find-application',
-    loadComponent: () =>
-      import(
-        './find-application/pages/find-application-page/find-application-page.component'
-      ).then((c) => c.FindApplicationPageComponent),
-  },
-  {
-    path: 'deliver-parcel',
-    loadComponent: () =>
-      import(
-        './deliver-parcel/pages/deliver-parcel-page/deliver-parcel-page.component'
-      ).then((c) => c.DeliverParcelPageComponent),
-  },
-  {
-    path: 'not-found',
-    loadComponent: () =>
-      import('./core/pages/not-found-page/not-found-page.component').then(
-        (c) => c.NotFoundPageComponent
+      import('./core/layouts/base-layout/base-layout.component').then(
+        (c) => c.BaseLayoutComponent
       ),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/find-application',
+      },
+      {
+        path: 'send-parcel',
+        loadComponent: () =>
+          import(
+            './send-parcel/pages/send-parcel-page/send-parcel-page.component'
+          ).then((c) => c.SendParcelPageComponent),
+      },
+      {
+        path: 'find-application',
+        loadComponent: () =>
+          import(
+            './find-application/pages/find-application-page/find-application-page.component'
+          ).then((c) => c.FindApplicationPageComponent),
+      },
+      {
+        path: 'deliver-parcel',
+        loadComponent: () =>
+          import(
+            './deliver-parcel/pages/deliver-parcel-page/deliver-parcel-page.component'
+          ).then((c) => c.DeliverParcelPageComponent),
+      },
+      {
+        path: 'not-found',
+        loadComponent: () =>
+          import('./core/pages/not-found-page/not-found-page.component').then(
+            (c) => c.NotFoundPageComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: '/not-found',
+      },
+    ],
   },
 ];
