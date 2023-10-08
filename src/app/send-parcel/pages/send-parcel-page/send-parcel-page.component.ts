96cdc54b1dbb4af9b5da68df13cb5f400820b4a0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 17:59:16 +0200

    Make a use of CreateHelpRequestFormComponent

--- src/app/send-parcel/pages/send-parcel-page/send-parcel-page.component.ts
+++ src/app/send-parcel/pages/send-parcel-page/send-parcel-page.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { CreateHelpRequestFormComponent } from '../../components/create-help-request-form/create-help-request-form.component';
+
 @Component({
   selector: 'pu-send-parcel-page',
   templateUrl: './send-parcel-page.component.html',
   styleUrls: ['./send-parcel-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [CreateHelpRequestFormComponent],
 })
 export class SendParcelPageComponent {}
