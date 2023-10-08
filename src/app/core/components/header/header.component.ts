c8b1a3d1763e7ba99d36a6c5970160c0bc2c7583
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-08 14:34:23 +0200

    Make a use of LocalizationSelectionContainerComponent

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { LocalizationSelectionContainerComponent } from '../localization-selection-container/localization-selection-container.component';
+
 @Component({
   selector: 'pu-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [LocalizationSelectionContainerComponent],
 })
 export class HeaderComponent {}
