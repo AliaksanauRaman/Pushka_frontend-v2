2b7d0f7b0180380978446caf6e411967b0c5f9f0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-09 20:38:20 +0200

    Create PlaceFieldStateName enum

---
+++ src/app/shared/components/place-field/place-field-state-name.enum.ts
@@ -0,0 +1,9 @@
+export enum PlaceFieldStateName {
+  INITIAL = 'initial',
+  PLACES_SET = 'places-set',
+  FIELD_INPUT = 'field-input',
+  WRITE_PLACE = 'write-place',
+  WRITE_NULL = 'write-null',
+  CLEAR = 'clear',
+  PLACE_SELECT = 'place-select',
+}
