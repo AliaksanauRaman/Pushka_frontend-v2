fe4d8b38ce7e0c8959d9d3554c674dadfb3e0494
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-12 13:07:19 +0200

    Finish base footer implementation

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { IconButtonComponent } from '@shared/components/icon-button/icon-button.component';
+
 @Component({
   selector: 'pu-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [IconButtonComponent],
 })
 export class FooterComponent {}
