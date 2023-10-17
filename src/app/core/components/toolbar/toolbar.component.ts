e27d9d0de7fb60f4641895f8a6a0fc4bb95c12bd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-17 16:40:44 +0200

    Enhance ToolbarComponent, add isSideMenuOpened input and _isSideMenuOpened signal field, add _menuButtonIconSrc, _menuButtonIconAlt and _menuButtonAriaLabel signal fields which are updated according to _isSideMenuOpened value

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  computed,
+  signal,
+} from '@angular/core';
 
 import { IconButtonComponent } from '@shared/components/icon-button/icon-button.component';
 
@@ -10,4 +18,25 @@ import { IconButtonComponent } from '@shared/components/icon-button/icon-button.
   standalone: true,
   imports: [IconButtonComponent],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  @Input()
+  public set isSideMenuOpened(value: boolean) {
+    this._isSideMenuOpened.set(value);
+  }
+
+  protected readonly _isSideMenuOpened = signal(false);
+  protected readonly _menuButtonIconSrc = computed(() =>
+    this._isSideMenuOpened()
+      ? '/assets/icons/white-cross-icon.svg'
+      : '/assets/icons/white-menu-icon.svg'
+  );
+  protected readonly _menuButtonIconAlt = computed(() =>
+    this._isSideMenuOpened() ? 'Close' : 'Menu'
+  );
+  protected readonly _menuButtonAriaLabel = computed(() =>
+    this._isSideMenuOpened() ? 'Close menu' : 'Open menu'
+  );
+
+  @Output()
+  public readonly menuClick = new EventEmitter<void>();
+}
