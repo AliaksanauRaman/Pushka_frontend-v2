9cbc7399c06bd46277fa77996ab1a9a90a595ef9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-17 10:24:17 +0200

    Add some cards to RequestsPageComponent

--- src/app/find-application/pages/requests-page/requests-page.component.ts
+++ src/app/find-application/pages/requests-page/requests-page.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ApplicationCardComponent } from '../../components/application-card/application-card.component';
+
 @Component({
   selector: 'pu-requests-page',
   templateUrl: './requests-page.component.html',
   styleUrls: ['./requests-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [ApplicationCardComponent],
 })
 export class RequestsPageComponent {}
