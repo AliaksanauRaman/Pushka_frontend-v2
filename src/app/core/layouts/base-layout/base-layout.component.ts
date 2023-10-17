2fd83b884c5476d4986a75c093da74ea1af15d6f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-17 16:56:57 +0200

    Update BaseLayoutComponent, add _isSideMenuOpened state, make a use of puSideMenu component

--- src/app/core/layouts/base-layout/base-layout.component.ts
+++ src/app/core/layouts/base-layout/base-layout.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { NgIf } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 import { ToolbarComponent } from '@core/components/toolbar/toolbar.component';
+import { SideMenuComponent } from '@core/components/side-menu/side-menu.component';
 import { RepresentationComponent } from '@core/components/representation/representation.component';
 import { NavigationComponent } from '@core/components/navigation/navigation.component';
 import { MainComponent } from '@core/components/main/main.component';
 import { FooterComponent } from '@core/components/footer/footer.component';
+
+import { ScrollingService } from '@shared/services/scrolling/scrolling.service';
 
 @Component({
   selector: 'pu-base-layout',
@@ -13,11 +23,39 @@ import { FooterComponent } from '@core/components/footer/footer.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
+    NgIf,
     ToolbarComponent,
+    SideMenuComponent,
     RepresentationComponent,
     NavigationComponent,
     MainComponent,
     FooterComponent,
   ],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ left: '100%' }),
+        animate('300ms ease-in-out', style({ left: '0%' })),
+      ]),
+      transition(
+        ':leave',
+        animate('300ms ease-in-out', style({ left: '100%' }))
+      ),
+    ]),
+  ],
 })
-export class BaseLayoutComponent {}
+export class BaseLayoutComponent {
+  private readonly _scrollingService = inject(ScrollingService);
+
+  protected readonly _isSideMenuOpened = signal(false);
+
+  protected openSideMenu(): void {
+    this._isSideMenuOpened.set(true);
+    this._scrollingService.disablePageScrolling();
+  }
+
+  protected closeSideMenu(): void {
+    this._isSideMenuOpened.set(false);
+    this._scrollingService.enablePageScrolling();
+  }
+}
