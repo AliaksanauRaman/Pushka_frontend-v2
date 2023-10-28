285839c05d2dd2c1a0a8b4b6045f1e0b50f3b9ad
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-10-28 16:01:08 +0200

    Add log in and account menu functionality to ToolbarComponent

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  signal,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { NgIf, NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 
 import { LocalizationSelectionContainerComponent } from '@core/components/localization-selection-container/localization-selection-container.component';
 import { SecondaryButtonComponent } from '@shared/components/secondary-button/secondary-button.component';
@@ -9,6 +18,28 @@ import { SecondaryButtonComponent } from '@shared/components/secondary-button/se
   styleUrls: ['./toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [LocalizationSelectionContainerComponent, SecondaryButtonComponent],
+  imports: [
+    NgIf,
+    NgOptimizedImage,
+    NgTemplateOutlet,
+    LocalizationSelectionContainerComponent,
+    SecondaryButtonComponent,
+    CdkMenu,
+    CdkMenuItem,
+    CdkMenuTrigger,
+  ],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  @Input()
+  public set isUserSignedIn(value: boolean) {
+    this._isUserSignedIn.set(value);
+  }
+
+  @Output()
+  public readonly logout = new EventEmitter<void>();
+
+  protected readonly _isUserSignedIn = signal(false);
+
+  // TODO: Temp
+  public email = 'test.user@gmail.com';
+}
