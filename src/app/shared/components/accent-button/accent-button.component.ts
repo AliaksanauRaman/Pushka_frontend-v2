00aaf8c0436f0b79d7fb006cb4adfd402627afaa
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-01 10:46:16 +0100

    Finish AccentButtonComponent implementation

--- src/app/shared/components/accent-button/accent-button.component.ts
+++ src/app/shared/components/accent-button/accent-button.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+  signal,
+} from '@angular/core';
+import { NgIf } from '@angular/common';
+
+import { SpinnerComponent } from '@shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'button[puAccentButton]',
@@ -6,5 +15,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./accent-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [NgIf, SpinnerComponent],
 })
-export class AccentButtonComponent {}
+export class AccentButtonComponent {
+  @Input()
+  public set isLoading(value: boolean) {
+    this._isLoading.set(value);
+  }
+
+  @HostBinding('attr.disabled')
+  public get hasDisabledAttr(): true | null {
+    return this._isLoading() ? true : null;
+  }
+
+  protected readonly _isLoading = signal(false);
+}
