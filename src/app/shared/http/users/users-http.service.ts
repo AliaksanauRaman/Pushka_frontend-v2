363849330ba0798b4a873d2df39f60677f49f86d
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-07 11:32:15 +0100

    Finish UsersHttpService implementation

--- src/app/shared/http/users/users-http.service.ts
+++ src/app/shared/http/users/users-http.service.ts
@@ -1,43 +1,33 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpParams, HttpStatusCode } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 import { Store } from '@ngxs/store';
-import { Observable, map, throwError } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
+import { IUsersHttpService } from './users-http.service.interface';
 import { BaseHttpService } from '@shared/base/base-http.service';
 import { RegisterDto } from '@shared/dtos/register.dto';
 import { SelectedLocalizationState } from '@store/selected-localization';
+import { RegisterResponseData } from '@shared/types/register-response-data';
+import { registerResponseData } from '@shared/unknown-types-parsers/register-response-data';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UsersHttpService extends BaseHttpService {
+export class UsersHttpService
+  extends BaseHttpService
+  implements IUsersHttpService
+{
   private readonly _store = inject(Store);
 
-  public register(registerDto: RegisterDto): Observable<unknown> {
+  public register(registerDto: RegisterDto): Observable<RegisterResponseData> {
     const selectedLocalization = this._store.selectSnapshot(
       SelectedLocalizationState.stream
     );
 
     return this._httpClient
-      .post<null>(`${this._apiUrl}/api/register`, registerDto.toMap(), {
+      .post<unknown>(`${this._apiUrl}/api/register`, registerDto.toMap(), {
         params: new HttpParams().append('lang', selectedLocalization.language),
-        observe: 'response',
       })
-      .pipe(
-        map((httpResponse) => {
-          const { status } = httpResponse;
-
-          if (status === HttpStatusCode.Ok) {
-            return httpResponse;
-          }
-
-          return throwError(
-            () =>
-              new Error(
-                `Unknown user register error! Status code: '${status}'.`
-              )
-          );
-        })
-      );
+      .pipe(map((responseData) => registerResponseData.parse(responseData)));
   }
 }
