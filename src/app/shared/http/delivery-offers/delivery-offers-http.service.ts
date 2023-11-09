5c75c1403bdbc59dc6245fae865b0321aa5264a2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-09 20:53:02 +0100

    Add initial implementation of DeliveryOffersHttpService.getPublished method

--- src/app/shared/http/delivery-offers/delivery-offers-http.service.ts
+++ src/app/shared/http/delivery-offers/delivery-offers-http.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { IDeliveryOffersHttpService } from './delivery-offers-http.interface';
 import { BaseHttpService } from '@shared/base/base-http.service';
+
+import { DeliveryOffersList } from '@shared/types/delivery-offer';
+import { ApplicationStatus } from '@shared/enums/application-status.enum';
 import { CreateDeliveryOfferDto } from '@shared/dtos/create-delivery-offer.dto';
 
 @Injectable({
@@ -14,9 +18,15 @@ export class DeliveryOffersHttpService
 {
   private readonly _deliveryOffersEndpoint = `${this._apiUrl}/api/help-offers`;
 
-  public getPublished(): Observable<unknown> {
-    return this._httpClient.get<unknown>(
-      `${this._deliveryOffersEndpoint}/published`
+  public getPublished(): Observable<DeliveryOffersList> {
+    return this._httpClient.get<DeliveryOffersList>(
+      `${this._deliveryOffersEndpoint}`,
+      {
+        params: new HttpParams()
+          .append('statuses', ApplicationStatus.PUBLISHED)
+          .append('page', 0)
+          .append('size', 10),
+      }
     );
   }
 
