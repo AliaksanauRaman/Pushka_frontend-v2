6c5b3299ee5d99b86ae91125fb49b068754d58a7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-09 18:59:35 +0100

    Update phoneNumberFormat validator

--- src/app/shared/validators/phone-number-format.validator.ts
+++ src/app/shared/validators/phone-number-format.validator.ts
@@ -1,19 +1,21 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
+import { Phone } from '@shared/types/phone';
+
 export const PHONE_NUMBER_REGEXP = /^[0-9]{7,12}$/;
 
 export const phoneNumberFormat = (
-  control: AbstractControl<string>
+  control: AbstractControl<Phone | null>
 ): ValidationErrors | null => {
   const { value } = control;
 
-  if (value === '') {
+  if (value === null) {
     return null;
   }
 
-  if (PHONE_NUMBER_REGEXP.test(value)) {
-    return null;
+  if (!PHONE_NUMBER_REGEXP.test(value.number)) {
+    return { phoneNumberFormat: true };
   }
 
-  return { phoneNumberFormat: true };
+  return null;
 };
