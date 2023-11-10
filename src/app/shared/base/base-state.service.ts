ab81d2cd2b775e4e646575d774133b04f394c008
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-10 11:34:38 +0100

    Start exposing state as signal from BaseStateService

--- src/app/shared/base/base-state.service.ts
+++ src/app/shared/base/base-state.service.ts
@@ -1,9 +1,11 @@
+import { toSignal } from '@angular/core/rxjs-interop';
 import { BehaviorSubject } from 'rxjs';
 
 export abstract class BaseStateService<T> {
   private readonly _state$ = new BehaviorSubject<T>(this.getInitialState());
 
   public readonly state$ = this._state$.asObservable();
+  public readonly state = toSignal(this.state$);
 
   protected updateState(state: T): void {
     this._state$.next(state);
