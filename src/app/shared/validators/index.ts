04f30ed30a63bc9ee02905d96d19ed19f9c83cfe
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-10 15:59:27 +0100

    Extend CustomValidators with all new validators

--- src/app/shared/validators/index.ts
+++ src/app/shared/validators/index.ts
@@ -2,7 +2,14 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 import { requiredDateRange } from './required-date-range.validator';
 import { emailFormat } from './email-format.validator';
+import { countryCodeRequired } from './country-code-required.validator';
+import { phoneNumberRequired } from './phone-number-required';
+import { phoneNumberFormat } from './phone-number-format.validator';
+import { atLeastOneLatinLetter } from './at-least-one-latin-letter.validator';
+import { atLeastOneDigit } from './at-least-one-digit.validator';
+import { passwordsMatch } from './passwords-match.validator';
 import { DateRange } from '@shared/types/date-range';
+import { Phone } from '@shared/types/phone';
 
 export class CustomValidators {
   public static requiredDateRange(
@@ -15,5 +22,41 @@ export class CustomValidators {
     control: AbstractControl<string>
   ): ValidationErrors | null {
     return emailFormat(control);
+  }
+
+  public static countryCodeRequired(
+    control: AbstractControl<Phone | null>
+  ): ValidationErrors | null {
+    return countryCodeRequired(control);
+  }
+
+  public static phoneNumberRequired(
+    control: AbstractControl<Phone | null>
+  ): ValidationErrors | null {
+    return phoneNumberRequired(control);
+  }
+
+  public static phoneNumberFormat(
+    control: AbstractControl<Phone | null>
+  ): ValidationErrors | null {
+    return phoneNumberFormat(control);
+  }
+
+  public static atLeastOneLatinLetter(
+    control: AbstractControl<string>
+  ): ValidationErrors | null {
+    return atLeastOneLatinLetter(control);
+  }
+
+  public static atLeastOneDigit(
+    control: AbstractControl<string>
+  ): ValidationErrors | null {
+    return atLeastOneDigit(control);
+  }
+
+  public static passwordsMatch(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    return passwordsMatch(control);
   }
 }
