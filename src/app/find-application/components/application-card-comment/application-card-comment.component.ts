78de7635d0a8baa9769df3b815e41572c21037f9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-10 13:22:39 +0100

    Add ApplicationCardService to ApplicationCardCommentComponent, remove old NgIf

--- src/app/find-application/components/application-card-comment/application-card-comment.component.ts
+++ src/app/find-application/components/application-card-comment/application-card-comment.component.ts
@@ -4,17 +4,19 @@ import {
   ChangeDetectorRef,
   Component,
   ElementRef,
-  Input,
+  OnInit,
   ViewChild,
+  computed,
   inject,
   signal,
 } from '@angular/core';
-import { NgIf } from '@angular/common';
 
 import { MATH } from '@global/math';
+import { ApplicationCardService } from '../application-card/application-card.service';
 import { GeneratorService } from '@shared/services/generator/generator.service';
 
 import { MAX_COLLAPSED_TEXT_HEIGHT } from './application-card-comment.config';
+import { Application } from '@shared/types/application';
 
 @Component({
   selector: 'pu-application-card-comment',
@@ -22,26 +24,29 @@ import { MAX_COLLAPSED_TEXT_HEIGHT } from './application-card-comment.config';
   styleUrls: ['./application-card-comment.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NgIf],
 })
-export class ApplicationCardCommentComponent implements AfterViewInit {
+export class ApplicationCardCommentComponent implements OnInit, AfterViewInit {
   private readonly _math = inject(MATH);
   private readonly _cdRef = inject(ChangeDetectorRef);
+  private readonly _applicationCardService = inject(ApplicationCardService);
   private readonly _maxCollapsedTextHeight = inject(MAX_COLLAPSED_TEXT_HEIGHT);
   protected readonly _expandTextButtonId =
     inject(GeneratorService).generateUUID();
 
-  @Input()
-  public set text(value: string) {
-    this._text.set(value);
-  }
-
   @ViewChild('text')
   private readonly _textElement!: ElementRef<HTMLParagraphElement>;
 
-  protected readonly _text = signal('');
+  private readonly _application = signal<Application | null>(null);
+  protected readonly _text = computed(() => {
+    const application = this._application();
+    return application === null ? '' : application.description;
+  });
   protected readonly _isExpandable = signal(false);
   protected readonly _isExpanded = signal(false);
+
+  public ngOnInit(): void {
+    this._application.set(this._applicationCardService.getApplication());
+  }
 
   public ngAfterViewInit(): void {
     this._isExpandable.set(
