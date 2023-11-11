4dfe6764c72e0c695b8f4a7e2ec11d91377e0904
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-11 14:28:13 +0100

    Add initialEmail and initialFullName @Input-s to RegisterFormComponent

--- src/app/core/components/register-form/register-form.component.ts
+++ src/app/core/components/register-form/register-form.component.ts
@@ -2,6 +2,8 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  Input,
+  OnInit,
   Output,
   inject,
 } from '@angular/core';
@@ -48,10 +50,20 @@ import { ValidRegisterFormValue } from '@shared/types/valid-register-form-value'
     AccentButtonComponent,
   ],
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
   private readonly _formBuilder = inject(NonNullableFormBuilder);
   private readonly _service = inject(RegisterFormService);
   protected readonly _config = inject(REGISTER_FORM_CONFIG);
+
+  @Input()
+  public set initialEmail(value: string) {
+    this._initialEmail = value;
+  }
+
+  @Input()
+  public set initialFullName(value: string) {
+    this._initialFullName = value;
+  }
 
   @Output()
   public readonly success = new EventEmitter<void>();
@@ -88,6 +100,15 @@ export class RegisterFormComponent {
       }
     })
   );
+  private _initialEmail = '';
+  private _initialFullName = '';
+
+  public ngOnInit(): void {
+    this._registerForm.patchValue({
+      email: this._initialEmail,
+      fullName: this._initialFullName,
+    });
+  }
 
   protected handleRegisterFormSubmit(): void {
     if (this._registerForm.invalid) {
