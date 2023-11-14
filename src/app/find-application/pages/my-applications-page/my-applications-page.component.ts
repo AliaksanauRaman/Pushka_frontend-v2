2dc117441c95ea0ccd08fb24a6f8098c1bbbe74a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-14 11:45:11 +0100

    Redirect user to 404 page from my applications page on logout

--- src/app/find-application/pages/my-applications-page/my-applications-page.component.ts
+++ src/app/find-application/pages/my-applications-page/my-applications-page.component.ts
@@ -1,4 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+} from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+
+import { UserState } from '@store/user';
+import { User } from '@shared/types/user';
 
 @Component({
   selector: 'pu-my-applications-page',
@@ -7,4 +19,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class MyApplicationsPageComponent {}
+export class MyApplicationsPageComponent {
+  private readonly _router = inject(Router);
+
+  @Select(UserState.stream)
+  private readonly _user$!: Observable<User | null>;
+  private readonly _user = toSignal(this._user$);
+  private readonly _redirectTo404PageOnUserLogout = effect(() => {
+    const user = this._user();
+
+    if (user !== null) {
+      return;
+    }
+
+    this._router.navigateByUrl('/find-application/requests');
+  });
+}
