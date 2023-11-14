ff71d54272e179fecb13b61b79cec483c4c7cfd3
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-14 11:30:22 +0100

    Add SnackBarConfig to SnackBarData, add default config

--- src/app/shared/types/snack-bar-data.ts
+++ src/app/shared/types/snack-bar-data.ts
@@ -1,3 +1,23 @@
+export type SnackBarConfig = Readonly<{
+  showClose: boolean;
+  actionLabel: string;
+}>;
+
+const DEFAULT_SNACK_BAR_CONFIG: SnackBarConfig = {
+  showClose: true,
+  actionLabel: '',
+};
+
 export class SnackBarData {
-  constructor(public readonly message: string) {}
+  public readonly config: SnackBarConfig;
+
+  constructor(
+    public readonly message: string,
+    config?: Partial<SnackBarConfig>
+  ) {
+    this.config = {
+      ...DEFAULT_SNACK_BAR_CONFIG,
+      ...config,
+    };
+  }
 }
