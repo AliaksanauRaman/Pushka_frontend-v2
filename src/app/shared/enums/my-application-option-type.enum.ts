bd7590af71b6d86a62b6edb529cd94eed3335bba
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-15 15:16:52 +0100

    Create shared MyApplicationOptionType enum which extends MyApplicationType

---
+++ src/app/shared/enums/my-application-option-type.enum.ts
@@ -0,0 +1,7 @@
+import { MyApplicationType } from './my-application-type.enum';
+
+export enum MyApplicationOptionType {
+  ALL = 'ALL',
+  REQUEST = MyApplicationType.REQUEST,
+  OFFER = MyApplicationType.OFFER,
+}
