dfb9d9597afe01540da4c846b6c813a50600c9b7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-16 19:58:15 +0100

    Add description comment to DATE_VIEW_FORMAT dependency

--- src/app/find-application/dependencies/date-view-format.ts
+++ src/app/find-application/dependencies/date-view-format.ts
@@ -1,5 +1,9 @@
 import { InjectionToken } from '@angular/core';
 
+/**
+ * Should be used with Angular DatePipe!
+ * More info: https://angular.io/api/common/DatePipe#custom-format-options
+ */
 export const DATE_VIEW_FORMAT = new InjectionToken('DATE_VIEW_FORMAT', {
   providedIn: 'root',
   factory: () => 'dd.MM.yyyy',
