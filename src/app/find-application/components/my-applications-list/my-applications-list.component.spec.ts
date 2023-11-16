045e1ffba5b798761046776e79e5ee9d74af37d8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-16 20:24:23 +0100

    Create MyApplicationsListComponent

---
+++ src/app/find-application/components/my-applications-list/my-applications-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyApplicationsListComponent } from './my-applications-list.component';
+
+describe('MyApplicationsListComponent', () => {
+  let component: MyApplicationsListComponent;
+  let fixture: ComponentFixture<MyApplicationsListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyApplicationsListComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MyApplicationsListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/find-application/components/my-applications-list/my-applications-list.component.ts
@@ -0,0 +1,40 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  signal,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { MyApplicationCardComponent } from '../my-application-card/my-application-card.component';
+import { TextLinkComponent } from '@shared/components/text-link/text-link.component';
+import { PlaceholderComponent } from '@shared/components/placeholder/placeholder.component';
+import { IconButtonComponent } from '@shared/components/icon-button/icon-button.component';
+
+import { BaseApplicationsListComponent } from '../../base/base-applications-list/base-applications-list.component';
+import { MyApplicationsList } from '@shared/types/my-application';
+
+@Component({
+  selector: 'pu-my-applications-list',
+  templateUrl: './my-applications-list.component.html',
+  styleUrl: '../../styles/_applications-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    RouterLink,
+    TranslateModule,
+    MyApplicationCardComponent,
+    TextLinkComponent,
+    PlaceholderComponent,
+    IconButtonComponent,
+  ],
+})
+export class MyApplicationsListComponent extends BaseApplicationsListComponent {
+  @Input({ required: true })
+  public set myApplicationsList(value: MyApplicationsList) {
+    this._myApplicationsList.set(value);
+  }
+
+  protected readonly _myApplicationsList = signal(new MyApplicationsList([]));
+}
