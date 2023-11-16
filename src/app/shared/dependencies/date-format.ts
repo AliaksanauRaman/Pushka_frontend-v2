fe037cd10ff4b87979e1ac49774dab3fc8ca8954
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-16 19:56:57 +0100

    Add description comment to DATE_FORMAT shared dependency

--- src/app/shared/dependencies/date-format.ts
+++ src/app/shared/dependencies/date-format.ts
@@ -1,5 +1,8 @@
 import { InjectionToken } from '@angular/core';
 
+/**
+ * Do not use it with DatePipe!
+ */
 export const DATE_FORMAT = new InjectionToken<string>('DATE_FORMAT', {
   providedIn: 'root',
   factory: () => 'DD.MM.YYYY',
