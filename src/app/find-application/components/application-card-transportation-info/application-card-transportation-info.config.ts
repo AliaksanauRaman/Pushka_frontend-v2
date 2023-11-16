e9f18c13fa17b26e821a9f2248cab947f3deef83
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-16 20:26:47 +0100

    Remove ApplicationCardTransportationInfoComponent

--- src/app/find-application/components/application-card-transportation-info/application-card-transportation-info.config.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const VIEW_DATE_FORMAT = new InjectionToken('VIEW_DATE_FORMAT', {
-  providedIn: 'root',
-  factory: () => 'dd.MM.yyyy',
-});
