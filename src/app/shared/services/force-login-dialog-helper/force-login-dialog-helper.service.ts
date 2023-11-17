3355e0d8c9975ade5def00bd59dccc525b16c166
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-17 16:37:09 +0100

    Update name

--- src/app/shared/services/force-login-dialog-helper/force-login-dialog-helper.service.ts
+++ src/app/shared/services/force-login-dialog-helper/force-login-dialog-helper.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { ForceLoginDialogComponent } from '@shared/dialogs/force-login-dialog/force-login-dialog.component';
 
-import { BaseDialogService } from '@shared/base/base-dialog.service';
+import { BaseDialogHelperService } from '@shared/base/base-dialog-helper.service';
 import { ForceLoginDialogData } from '@shared/types/force-login-dialog-data';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ForceLoginDialogHelperService extends BaseDialogService {
+export class ForceLoginDialogHelperService extends BaseDialogHelperService {
   public openDialog(data: ForceLoginDialogData): void {
     this._dialog.open(ForceLoginDialogComponent, {
       data,
