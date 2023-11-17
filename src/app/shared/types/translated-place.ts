2a737e97d6a0ea092aab5212bb637f2b34d176f6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-17 15:46:37 +0100

    Add missing methods of TranslatedPlace

--- src/app/shared/types/translated-place.ts
+++ src/app/shared/types/translated-place.ts
@@ -1,13 +1,29 @@
 import { Place } from './place';
 import { Country } from './country';
 import { City } from './city';
+import { IComparable } from '@shared/interfaces/comparable';
 
-export class TranslatedPlace extends Place {
+export class TranslatedPlace
+  extends Place
+  implements IComparable<TranslatedPlace>
+{
+  public static override is(value: unknown): value is TranslatedPlace {
+    return value instanceof TranslatedPlace;
+  }
+
   constructor(
     { countryId, countryLabel, cityId, cityLabel }: Place,
     public readonly plainCountryLabel: string,
     public readonly plainCityLabel: string
   ) {
     super(new Country(countryId, countryLabel), new City(cityId, cityLabel));
+  }
+
+  public override equalsTo(place: TranslatedPlace): boolean {
+    return (
+      this.plainCityLabel === place.plainCityLabel &&
+      this.plainCountryLabel === place.plainCountryLabel &&
+      super.equalsTo(place)
+    );
   }
 }
