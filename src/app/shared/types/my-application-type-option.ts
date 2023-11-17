ce87a3333b99bafe2eb68b2bdf360476a5ca1c43
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-17 16:33:05 +0100

    Make MyApplicationTypeOption comparable

--- src/app/shared/types/my-application-type-option.ts
+++ src/app/shared/types/my-application-type-option.ts
@@ -1,8 +1,15 @@
 import { MyApplicationOptionType } from '@shared/enums/my-application-option-type.enum';
+import { IComparable } from '@shared/interfaces/comparable';
 
-export class MyApplicationTypeOption {
+export class MyApplicationTypeOption
+  implements IComparable<MyApplicationTypeOption>
+{
   constructor(
     public readonly type: MyApplicationOptionType,
     public readonly label: string
   ) {}
+
+  public equalsTo({ type, label }: MyApplicationTypeOption): boolean {
+    return this.type === type && this.label === label;
+  }
 }
