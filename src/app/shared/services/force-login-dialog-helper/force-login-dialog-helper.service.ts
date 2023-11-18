51b40f3bac41eb4a2f17fd29bb46917ca2df9440
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-18 14:47:26 +0100

    Update ForceLoginDialogHelperService, add the possibility to handle dialog result

--- src/app/shared/services/force-login-dialog-helper/force-login-dialog-helper.service.ts
+++ src/app/shared/services/force-login-dialog-helper/force-login-dialog-helper.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { ForceLoginDialogComponent } from '@shared/dialogs/force-login-dialog/force-login-dialog.component';
 
 import { BaseDialogHelperService } from '@shared/base/base-dialog-helper.service';
 import { ForceLoginDialogData } from '@shared/types/force-login-dialog-data';
+import { ForceLoginDialogResult } from '@shared/types/force-login-dialog-result';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ForceLoginDialogHelperService extends BaseDialogHelperService {
-  public openDialog(data: ForceLoginDialogData): void {
-    this._dialog.open(ForceLoginDialogComponent, {
+  public openDialog(data: ForceLoginDialogData): Observable<ForceLoginDialogResult> {
+    return this._dialog.open<
+      ForceLoginDialogResult,
+      ForceLoginDialogData,
+      ForceLoginDialogComponent
+    >(ForceLoginDialogComponent, {
       data,
-    });
+    }).closed;
   }
 }
