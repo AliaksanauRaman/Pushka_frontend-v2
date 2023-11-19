c2af4f7b3da4d0ca464cfc16def9526b887823db
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-19 16:35:49 +0100

    Extend FilterByPlaceValue with withNewTranslations, withDeparturePlace, withDestination and equalsTo methods

--- src/app/shared/types/filter-by-place-value.ts
+++ src/app/shared/types/filter-by-place-value.ts
@@ -1,6 +1,7 @@
 import { TranslatedPlace } from './translated-place';
+import { IComparable } from '@shared/interfaces/comparable';
 
-export class FilterByPlaceValue {
+export class FilterByPlaceValue implements IComparable<FilterByPlaceValue> {
   public get isEmpty(): boolean {
     return this.departurePlace === null && this.destination === null;
   }
@@ -9,4 +10,42 @@ export class FilterByPlaceValue {
     public readonly departurePlace: TranslatedPlace | null,
     public readonly destination: TranslatedPlace | null
   ) {}
+
+  // TODO: Not used
+  public withNewTranslations(
+    translatedPlaces: ReadonlyArray<TranslatedPlace>
+  ): FilterByPlaceValue {
+    const { departurePlace, destination } = this;
+    const newDeparturePlace =
+      departurePlace !== null
+        ? translatedPlaces.find(({ id }) => departurePlace.id === id)
+        : undefined;
+    const newDestination =
+      destination !== null
+        ? translatedPlaces.find(({ id }) => destination.id === id)
+        : undefined;
+    return new FilterByPlaceValue(
+      newDeparturePlace || null,
+      newDestination || null
+    );
+  }
+
+  public withDeparturePlace(
+    departurePlace: TranslatedPlace | null
+  ): FilterByPlaceValue {
+    return new FilterByPlaceValue(departurePlace, this.destination);
+  }
+
+  public withDestination(
+    destination: TranslatedPlace | null
+  ): FilterByPlaceValue {
+    return new FilterByPlaceValue(this.departurePlace, destination);
+  }
+
+  public equalsTo(value: FilterByPlaceValue): boolean {
+    return (
+      this.departurePlace?.id === value.departurePlace?.id &&
+      this.destination?.id === value.destination?.id
+    );
+  }
 }
