af0b59407dcf8c77abe4299f8196519067305150
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-20 22:24:24 +0100

    Add toAuthorizedDto and toUnauthorizedDto methods to ValidDeliverParcelFormValue instead of toCreateDeliveryOfferDto

--- src/app/shared/types/valid-deliver-parcel-form-value.ts
+++ src/app/shared/types/valid-deliver-parcel-form-value.ts
@@ -2,7 +2,8 @@ import { Place } from './place';
 import { DateRange } from './date-range';
 import { Phone } from './phone';
 import { ApplicationStatus } from '@shared/enums/application-status.enum';
-import { CreateDeliveryOfferDto } from '@shared/dtos/create-delivery-offer.dto';
+import { AuthorizedCreateDeliveryOfferDto } from '@shared/dtos/authorized-create-delivery-offer.dto';
+import { UnauthorizedCreateDeliveryOfferDto } from '@shared/dtos/unauthorized-create-delivery-offer.dto';
 
 type RawDeliverParcelFormValue = Readonly<{
   departurePlace: Place | null;
@@ -53,10 +54,8 @@ export class ValidDeliverParcelFormValue {
       noServiceResponsibilityConfirmation;
   }
 
-  public toCreateDeliveryOfferDto(
-    status: ApplicationStatus
-  ): CreateDeliveryOfferDto {
-    return new CreateDeliveryOfferDto(
+  public toAuthorizedDto(): AuthorizedCreateDeliveryOfferDto {
+    return new AuthorizedCreateDeliveryOfferDto(
       this.departurePlace.id,
       this.destination.id,
       this.validityPeriodStart,
@@ -65,7 +64,22 @@ export class ValidDeliverParcelFormValue {
       this.fullName,
       this.email,
       this.phone,
-      status
+      ApplicationStatus.PUBLISHED
+    );
+  }
+
+  public toUnauthorizedDto(userId: number): UnauthorizedCreateDeliveryOfferDto {
+    return new UnauthorizedCreateDeliveryOfferDto(
+      userId,
+      this.departurePlace.id,
+      this.destination.id,
+      this.validityPeriodStart,
+      this.validityPeriodEnd,
+      this.description,
+      this.fullName,
+      this.email,
+      this.phone,
+      ApplicationStatus.PENDING
     );
   }
 }
