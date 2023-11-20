e24fe2a224b59f7788c33d577a6b85cec2cebff2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-20 12:07:16 +0100

    Make a use of DeleteDeliveryOfferService in DeliveryOfferCardComponent

--- src/app/find-application/components/delivery-offer-card/delivery-offer-card.component.ts
+++ src/app/find-application/components/delivery-offer-card/delivery-offer-card.component.ts
@@ -1,15 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  DestroyRef,
   EventEmitter,
   Input,
   Output,
   inject,
   signal,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { catchError, finalize, tap, throwError } from 'rxjs';
+import { tap } from 'rxjs';
 
 import { ApplicationCardHeaderComponent } from '../application-card-header/application-card-header.component';
 import { DeliveryOfferCardTransportationInfoComponent } from '../delivery-offer-card-transportation-info/delivery-offer-card-transportation-info.component';
@@ -18,8 +16,7 @@ import { ApplicationCardUserDetailsComponent } from '../application-card-user-de
 import { SpinnerComponent } from '@shared/components/spinner/spinner.component';
 import { ApplicationDirective } from '../../directives/application/application.directive';
 
-import { DeliveryOffersHttpService } from '@shared/http/delivery-offers/delivery-offers-http.service';
-import { SnackBarService } from '@shared/services/snack-bar/snack-bar.service';
+import { DeleteDeliveryOfferService } from '../../services/delete-delivery-offer/delete-delivery-offer.service';
 
 import { Application } from '@shared/types/application';
 import { MyApplicationType } from '@shared/enums/my-application-type.enum';
@@ -29,6 +26,7 @@ import { MyApplicationType } from '@shared/enums/my-application-type.enum';
   templateUrl: './delivery-offer-card.component.html',
   styleUrl: '../../styles/_application-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DeleteDeliveryOfferService],
   standalone: true,
   imports: [
     ApplicationCardHeaderComponent,
@@ -45,11 +43,7 @@ import { MyApplicationType } from '@shared/enums/my-application-type.enum';
   ],
 })
 export class DeliveryOfferCardComponent {
-  private readonly _destroyRef = inject(DestroyRef);
-  private readonly _deliveryOffersHttpService = inject(
-    DeliveryOffersHttpService
-  );
-  private readonly _snackBarService = inject(SnackBarService);
+  protected readonly _deleteDeliveryOffer = inject(DeleteDeliveryOfferService);
   protected readonly _application =
     inject<ApplicationDirective<Application>>(ApplicationDirective);
 
@@ -63,24 +57,11 @@ export class DeliveryOfferCardComponent {
 
   protected readonly _isMine = signal(false);
   protected readonly _type = MyApplicationType.OFFER;
-  protected readonly _isDeleting = signal(false);
 
   protected deleteDeliveryOffer(id: number): void {
-    this._isDeleting.set(true);
-
-    this._deliveryOffersHttpService
-      .deleteOne(id)
-      .pipe(
-        tap(() => this.deleted.emit()),
-        catchError((error: unknown) => {
-          this._snackBarService.showErrorMessage(
-            'backendError.unknownDeliveryOfferDeletionError'
-          );
-          return throwError(() => error);
-        }),
-        finalize(() => this._isDeleting.set(false)),
-        takeUntilDestroyed(this._destroyRef)
-      )
+    this._deleteDeliveryOffer
+      .deleteById(id)
+      .pipe(tap(() => this.deleted.emit()))
       .subscribe();
   }
 }
