4659c6e076ae1086fbeea49cb27308a0e424c25b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-20 10:29:56 +0100

    Add deletion confirmation dialog to ApplicationCardHeaderComponent

--- src/app/find-application/components/application-card-header/application-card-header.component.ts
+++ src/app/find-application/components/application-card-header/application-card-header.component.ts
@@ -1,12 +1,20 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
+  EventEmitter,
   Input,
+  Output,
+  inject,
   signal,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter, tap } from 'rxjs';
 
 import { ApplicationCardTagComponent } from '../application-card-tag/application-card-tag.component';
 import { IconButtonComponent } from '@shared/components/icon-button/icon-button.component';
+
+import { DeleteApplicationDialogHelperService } from '@shared/services/delete-application-dialog-helper/delete-application-dialog-helper.service';
 
 import { MyApplicationType } from '@shared/enums/my-application-type.enum';
 
@@ -19,10 +27,29 @@ import { MyApplicationType } from '@shared/enums/my-application-type.enum';
   imports: [ApplicationCardTagComponent, IconButtonComponent],
 })
 export class ApplicationCardHeaderComponent {
+  private readonly _destroyRef = inject(DestroyRef);
+  private readonly _deleteApplicationDialogHelperService = inject(
+    DeleteApplicationDialogHelperService
+  );
+
   @Input()
   public set applicationType(value: MyApplicationType) {
     this._applicationType.set(value);
   }
 
+  @Output()
+  public readonly deletionConfirm = new EventEmitter<void>();
+
   protected readonly _applicationType = signal(MyApplicationType.OFFER);
+
+  protected openDeleteApplicationDialog(): void {
+    this._deleteApplicationDialogHelperService
+      .openDialog()
+      .pipe(
+        filter((isConfirmed): isConfirmed is true => isConfirmed === true),
+        tap(() => this.deletionConfirm.emit()),
+        takeUntilDestroyed(this._destroyRef)
+      )
+      .subscribe();
+  }
 }
