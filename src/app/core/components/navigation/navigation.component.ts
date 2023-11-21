81b708bbf5d53f31f2325349f3bbddd7b36d74f7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-21 13:52:37 +0100

    Enhance NavigationComponent

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { NgFor } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -8,10 +7,10 @@ import { NAVIGATION_LINKS } from './navigation.config';
 @Component({
   selector: 'nav[puNavigation]',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
+  styleUrl: './navigation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NgFor, RouterLink, RouterLinkActive, TranslateModule],
+  imports: [RouterLink, RouterLinkActive, TranslateModule],
 })
 export class NavigationComponent {
   protected readonly _navigationLinks = inject(NAVIGATION_LINKS);
