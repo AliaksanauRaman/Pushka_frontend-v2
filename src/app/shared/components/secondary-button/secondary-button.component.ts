d8c732a8dce47e968cb9f551eeda899854970df8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-21 13:09:16 +0100

    Remove redundant parts of SecondaryButtonComponent, adapt it for laptops

--- src/app/shared/components/secondary-button/secondary-button.component.ts
+++ src/app/shared/components/secondary-button/secondary-button.component.ts
@@ -1,15 +1,10 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'button[puSecondaryButton]',
-  templateUrl: './secondary-button.component.html',
-  styleUrls: ['./secondary-button.component.scss'],
+  template: '<ng-content></ng-content>',
+  styleUrl: './secondary-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class SecondaryButtonComponent {
-  @HostBinding('attr.type')
-  public get type(): 'button' {
-    return 'button';
-  }
-}
+export class SecondaryButtonComponent {}
