87538369f3dc4e88a2597f90d40ff0d1a1580408
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-21 13:30:40 +0100

    Add images to representation component via css instead of html, adapt component for laptops

--- src/app/core/components/representation/representation.component.ts
+++ src/app/core/components/representation/representation.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'section[puRepresentation]',
   templateUrl: './representation.component.html',
-  styleUrls: ['./representation.component.scss'],
+  styleUrl: './representation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NgOptimizedImage, TranslateModule],
+  imports: [TranslateModule],
 })
 export class RepresentationComponent {}
