a27bd0a12ed670a27f79464c35b377b389763787
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-24 21:28:20 +0100

    Create ActivateUserPlaceholderComponent

---
+++ src/app/activate-user/components/activate-user-placeholder/activate-user-placeholder.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  template: '',
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class ActivateUserPlaceholderComponent {}
