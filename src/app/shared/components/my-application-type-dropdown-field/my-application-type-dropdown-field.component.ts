caf0c5ce120f1f411f634cf35127d1a94f45113f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-24 17:44:01 +0100

    Extend MyApplicationTypeDropdownFieldComponent from BaseMyApplicationTypeFieldComponent, remove NgOptimizedImage from there

--- src/app/shared/components/my-application-type-dropdown-field/my-application-type-dropdown-field.component.ts
+++ src/app/shared/components/my-application-type-dropdown-field/my-application-type-dropdown-field.component.ts
@@ -1,22 +1,13 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-  inject,
-  signal,
-} from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { PanelComponent } from '@shared/components/panel/panel.component';
 import { PanelItemComponent } from '@shared/components/panel-item/panel-item.component';
-import { DisabledDirective } from '@shared/directives/disabled.directive';
 
 import { PanelService } from '@shared/services/panel/panel.service';
 
+import { BaseMyApplicationTypeFieldComponent } from '@shared/base/base-my-application-type-field.component';
 import { MyApplicationTypeOption } from '@shared/types/my-application-type-option';
 
 @Component({
@@ -30,43 +21,15 @@ import { MyApplicationTypeOption } from '@shared/types/my-application-type-optio
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
-    NgOptimizedImage,
     CdkOverlayOrigin,
     CdkConnectedOverlay,
     TranslateModule,
     PanelComponent,
     PanelItemComponent,
-    DisabledDirective,
   ],
 })
-export class MyApplicationTypeDropdownFieldComponent {
+export class MyApplicationTypeDropdownFieldComponent extends BaseMyApplicationTypeFieldComponent {
   protected readonly _panel = inject(PanelService);
-
-  @Input({ required: true })
-  public set options(value: ReadonlyArray<MyApplicationTypeOption>) {
-    this._options.set(value);
-  }
-
-  @Input()
-  public set selectedOption(value: MyApplicationTypeOption) {
-    this._selectedOption.set(value);
-  }
-
-  @Input()
-  public set isDisabled(value: boolean) {
-    this._isDisabled.set(value);
-  }
-
-  @Output()
-  public readonly optionSelect = new EventEmitter<MyApplicationTypeOption>();
-
-  protected readonly _options = signal<ReadonlyArray<MyApplicationTypeOption>>(
-    []
-  );
-  protected readonly _selectedOption = signal<MyApplicationTypeOption | null>(
-    null
-  );
-  protected readonly _isDisabled = signal(false);
 
   protected selectOption(option: MyApplicationTypeOption): void {
     this._panel.close();
