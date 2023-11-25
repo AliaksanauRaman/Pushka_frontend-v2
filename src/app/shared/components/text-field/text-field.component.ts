bcdec70d81652d9648e8585a762c7920ec2fff1a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-25 10:05:37 +0100

    Add AutocompleteDirective to TextFieldComponent, remove isCapitalized from there

--- src/app/shared/components/text-field/text-field.component.ts
+++ src/app/shared/components/text-field/text-field.component.ts
@@ -4,14 +4,16 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { IdDirective } from '@shared/directives/id.directive';
 import { LabelDirective } from '@shared/directives/label.directive';
 import { PlaceholderDirective } from '@shared/directives/placeholder.directive';
+import { AutocompleteDirective } from '@shared/directives/autocomplete.directive';
+
 import { BaseTextFieldDirective } from '@shared/base/base-text-field.directive';
 
 @Component({
   selector: 'pu-text-field',
   templateUrl: './text-field.component.html',
   styleUrls: [
-    './text-field.component.scss',
     '../../../styles/components/_field.component.scss',
+    './text-field.component.scss',
   ],
   providers: [
     {
@@ -26,14 +28,7 @@ import { BaseTextFieldDirective } from '@shared/base/base-text-field.directive';
     { directive: IdDirective, inputs: ['puId'] },
     { directive: LabelDirective, inputs: ['puLabel'] },
     { directive: PlaceholderDirective, inputs: ['puPlaceholder'] },
+    { directive: AutocompleteDirective, inputs: ['puAutocomplete'] },
   ],
 })
-export class TextFieldComponent extends BaseTextFieldDirective {
-  // TODO: Remove
-  @Input()
-  public set isCapitalized(value: boolean) {
-    this._isCapitalized.set(value);
-  }
-
-  protected readonly _isCapitalized = signal(false);
-}
+export class TextFieldComponent extends BaseTextFieldDirective {}
