d224096a481db4bc89dd98560e972d30bf2ce1f0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-25 11:57:35 +0100

    Add some hover states to the app

--- src/app/shared/components/panel-item/panel-item.component.ts
+++ src/app/shared/components/panel-item/panel-item.component.ts
@@ -19,10 +19,21 @@ export class PanelItemComponent {
     this._isPlaceholder.set(value);
   }
 
+  @Input()
+  public set isSelected(value: boolean) {
+    this._isSelected.set(value);
+  }
+
   @HostBinding('class.pu-panel-item--placeholder')
   public get hasPuPanelItemPlaceholderClass(): boolean {
     return this._isPlaceholder();
   }
 
+  @HostBinding('class.pu-panel-item--selected')
+  public get hasPuPanelItemSelectedClass(): boolean {
+    return this._isSelected();
+  }
+
   protected readonly _isPlaceholder = signal(false);
+  protected readonly _isSelected = signal(false);
 }
