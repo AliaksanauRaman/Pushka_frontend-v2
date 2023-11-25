6f837623cad40d33fae176fbfe9ebf576874c4e5
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-25 10:02:10 +0100

    Inject AutocompleteDirective to BaseTextFieldDirective

--- src/app/shared/base/base-text-field.directive.ts
+++ src/app/shared/base/base-text-field.directive.ts
@@ -4,19 +4,22 @@ import { z } from 'zod';
 import { IdDirective } from '@shared/directives/id.directive';
 import { LabelDirective } from '@shared/directives/label.directive';
 import { PlaceholderDirective } from '@shared/directives/placeholder.directive';
+import { AutocompleteDirective } from '@shared/directives/autocomplete.directive';
 
 import { BaseReactiveFieldDirective } from './base-reactive-field.directive';
 
 @Directive()
 export abstract class BaseTextFieldDirective extends BaseReactiveFieldDirective<string> {
+  protected readonly _idDirective = inject(IdDirective);
+  protected readonly _labelDirective = inject(LabelDirective);
+  protected readonly _placeholderDirective = inject(PlaceholderDirective);
+  protected readonly _autocomplete = inject(AutocompleteDirective);
+
   @Input()
   public set puMaxLength(value: number) {
     this._maxLength.set(value);
   }
 
-  protected readonly _idDirective = inject(IdDirective);
-  protected readonly _labelDirective = inject(LabelDirective);
-  protected readonly _placeholderDirective = inject(PlaceholderDirective);
   protected readonly _maxLength = signal(30);
   protected readonly _value = signal('');
 
