18521d45ed26a98346a9f92e53ba682022427997
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-26 18:14:26 +0100

    Show under testing dialog once a user enters our app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { UnderTestingDialogHelperService } from '@core/services/under-testing-dialog-helper/under-testing-dialog-helper.service';
 
 @Component({
   selector: 'pu-root',
@@ -9,4 +16,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly _underTestingDialogHelper = inject(
+    UnderTestingDialogHelperService
+  );
+
+  public ngOnInit(): void {
+    this._underTestingDialogHelper.openDialogIfNotViewed();
+  }
+}
