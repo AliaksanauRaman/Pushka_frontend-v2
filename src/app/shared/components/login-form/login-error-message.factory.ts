6d004ea69fb0d024d8e4706f97475c80bfc303f8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-26 17:36:46 +0100

    Enhance LoginErrorMessageFactory, add ACCOUNT_IS_NOT_ACTIVATED error handling

--- src/app/shared/components/login-form/login-error-message.factory.ts
+++ src/app/shared/components/login-form/login-error-message.factory.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { errorWithCode } from '@shared/unknown-types-parsers/error-with-code';
+
+enum LoginErrorCode {
+  ACCOUNT_IS_NOT_ACTIVATED = 10,
+  INVALID_CREDENTIALS = 11,
+}
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +17,12 @@ export class LoginErrorMessageFactory {
       return 'backendError.unknownLoginError';
     }
 
-    const { status } = errorResponse;
+    const { code } = errorWithCode.parse(errorResponse.error);
 
-    switch (status) {
-      case HttpStatusCode.Unauthorized:
+    switch (code) {
+      case LoginErrorCode.INVALID_CREDENTIALS:
         return 'backendError.invalidCredentials';
-      // For the future. Currently does not work
-      case HttpStatusCode.Forbidden:
+      case LoginErrorCode.ACCOUNT_IS_NOT_ACTIVATED:
         return 'backendError.accountIsNotActivated';
       default:
         return 'backendError.unknownLoginError';
