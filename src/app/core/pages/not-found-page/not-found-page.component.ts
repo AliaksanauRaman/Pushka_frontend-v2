cce119f6453eb9e3ac947cb200826deda56a0c33
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-26 14:36:52 +0100

    Finish 404 page

--- src/app/core/pages/not-found-page/not-found-page.component.ts
+++ src/app/core/pages/not-found-page/not-found-page.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { AccentButtonComponent } from '@shared/components/accent-button/accent-button.component';
 
 @Component({
   selector: 'pu-not-found-page',
@@ -6,5 +10,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./not-found-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [RouterLink, TranslateModule, AccentButtonComponent],
 })
 export class NotFoundPageComponent {}
