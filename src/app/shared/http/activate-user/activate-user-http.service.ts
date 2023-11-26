25c77d8ca476f3cb7c7aeab5c89b7d351b24ee73
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-26 17:33:46 +0100

    Remove excess part of ActivateUserHttpService.activateUser method

--- src/app/shared/http/activate-user/activate-user-http.service.ts
+++ src/app/shared/http/activate-user/activate-user-http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpParams, HttpStatusCode } from '@angular/common/http';
-import { Observable, map, throwError } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { BaseHttpService } from '@shared/base/base-http.service';
 
@@ -9,30 +9,13 @@ import { BaseHttpService } from '@shared/base/base-http.service';
 })
 export class ActivateUserHttpService extends BaseHttpService {
   public activateUser(token: string): Observable<unknown> {
-    return this._httpClient
-      .post<null>(
-        `${this._apiUrl}/api/registration/confirm`,
-        {},
-        {
-          params: new HttpParams().set('token', token),
-          observe: 'response',
-        }
-      )
-      .pipe(
-        map((httpResponse) => {
-          const { status } = httpResponse;
-
-          if (status === HttpStatusCode.Ok) {
-            return httpResponse;
-          }
-
-          return throwError(
-            () =>
-              new Error(
-                `Unknown user activation error! Status code: '${status}'.`
-              )
-          );
-        })
-      );
+    return this._httpClient.post<null>(
+      `${this._apiUrl}/api/registration/confirm`,
+      {},
+      {
+        params: new HttpParams().set('token', token),
+        observe: 'response',
+      }
+    );
   }
 }
