8d068c4dce98f2fddec0170233f507b97a765b01
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-28 19:25:19 +0100

    Add isSpecific getter for City

--- src/app/shared/types/city.ts
+++ src/app/shared/types/city.ts
@@ -1,8 +1,14 @@
 import { IComparable } from '@shared/interfaces/comparable';
 
+const OTHER_CITY_LABEL = 'city.other';
+
 export class City implements IComparable<City> {
   public get isOther(): boolean {
-    return this.label === 'city.other';
+    return this.label === OTHER_CITY_LABEL;
+  }
+
+  public get isSpecific(): boolean {
+    return this.label !== OTHER_CITY_LABEL;
   }
 
   constructor(public readonly id: number, public readonly label: string) {}
