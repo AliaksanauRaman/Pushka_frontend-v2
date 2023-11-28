f36f58430ab5677d7ca8b2a8262d016d9d1cb121
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-11-28 16:24:14 +0100

    Enhance PlaceholderComponent, remove separate files for template and styles, as they are not needed, remove redundant tests

--- src/app/shared/components/placeholder/placeholder.component.ts
+++ src/app/shared/components/placeholder/placeholder.component.ts
@@ -2,8 +2,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'pu-placeholder',
-  templateUrl: './placeholder.component.html',
-  styleUrl: './placeholder.component.scss',
+  template: '',
+  styles: `
+    @import '../../../styles/_screen-size-mixins.scss';
+
+    :host {
+      display: block;
+      background: #D9D9D9;
+      border-radius: 12px;
+    }
+
+    @include for-laptop {
+      :host {
+        border-radius: 16px;
+      }
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
