facb0a038abbaea88f8ea785f5b4551b8deb09c4
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-12-01 14:39:39 +0100

    Remove the functionality to add theme color meta tag in UserEntryDialogHelperService, update theme_color in webmanifest, return transition for cdk backdrops

--- src/app/core/services/user-entry-dialog-helper/user-entry-dialog-helper.service.ts
+++ src/app/core/services/user-entry-dialog-helper/user-entry-dialog-helper.service.ts
@@ -1,30 +1,16 @@
-import { Injectable, inject } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
 
 import { UserEntryDialogComponent } from '@core/dialogs/user-entry-dialog/user-entry-dialog.component';
 
 import { BaseDialogHelperService } from '@shared/base/base-dialog-helper.service';
 
-// TODO
-// <meta content="#1f3c47" data-react-helmet="true" name="theme-color" />
 @Injectable()
 export class UserEntryDialogHelperService extends BaseDialogHelperService {
-  private readonly _meta = inject(Meta);
-
   public openDialog(): void {
-    this._meta.addTag({
-      content: '#1f3c47',
-      name: 'theme-color',
-      'data-react-helmet': 'true',
-    });
-    const dialogRef = this._dialog.open(UserEntryDialogComponent, {
+    this._dialog.open(UserEntryDialogComponent, {
       width: '664px',
       autoFocus: false,
       panelClass: 'pu-dialog--scrollable',
-    });
-
-    dialogRef.closed.subscribe({
-      next: () => this._meta.removeTag('data-react-helmet=true'),
     });
   }
 }
