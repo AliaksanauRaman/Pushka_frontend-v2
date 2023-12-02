5d5bd1f4cc6669edd86e54145b58a7b6e41184a7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-12-02 19:18:11 +0100

    Add DIALOG_CONFIG to BaseDialogHelperService

--- src/app/shared/base/base-dialog-helper.service.ts
+++ src/app/shared/base/base-dialog-helper.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, inject } from '@angular/core';
 import { Dialog } from '@angular/cdk/dialog';
 
+import { DIALOG_CONFIG } from '@shared/dependencies/dialog.config';
+
 @Injectable()
 export abstract class BaseDialogHelperService {
   protected readonly _dialog = inject(Dialog);
+  protected readonly _config = inject(DIALOG_CONFIG);
 }
